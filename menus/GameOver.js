4b05e22fe109757e436cc0cea75b9ee40f1983bf
Marco Koivisto <marcokoivisto@me.com>
2019-11-22 12:33:14 +0100

    Some removes..

--- menus/GameOver.js
+++ menus/GameOver.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import { StyleSheet, View, Text, SafeAreaView } from "react-native";
 import { useHistory } from "react-router-native";
 import PropTypes from "prop-types";
